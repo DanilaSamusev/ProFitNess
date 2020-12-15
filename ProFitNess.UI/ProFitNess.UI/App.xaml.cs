4e75642e9e87c568912b3b8976de3cf002c1b0e8
Danila Samuseu <Danila_Samuseu@epam.com>
2020-12-15 23:58:59 +0300

    Initial commit

---
+++ ProFitNess.UI/ProFitNess.UI/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ProFitNess.UI.Views;
+
+using Xamarin.Forms;
+
+namespace ProFitNess.UI
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new HomePage());
+        }
+    }
+}
