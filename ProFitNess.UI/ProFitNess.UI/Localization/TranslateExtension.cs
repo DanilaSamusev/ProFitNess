13ad73b3048b7b5cba29db8b804e2760d2e797f4
Danila Samuseu <Danila_Samuseu@epam.com>
2020-12-20 20:34:21 +0300

    Added localization logic. Localiazed all slides

---
+++ ProFitNess.UI/ProFitNess.UI/Localization/TranslateExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Resources;
+using System.Globalization;
+using System.Reflection;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProFitNess.UI.Localization
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        private readonly CultureInfo _cultureInfo;
+        private const string ResourcePath = "ProFitNess.UI.Localization.Resource";
+
+        public TranslateExtension()
+        {
+            _cultureInfo = new CultureInfo("ru");
+        }
+
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+            {
+                return string.Empty;
+            }
+
+            var resourceManager = new ResourceManager(ResourcePath, typeof(TranslateExtension).GetTypeInfo().Assembly);
+            var translation = resourceManager.GetString(Text, _cultureInfo) ?? Text;
+
+            return translation;
+        }
+    }
+}
